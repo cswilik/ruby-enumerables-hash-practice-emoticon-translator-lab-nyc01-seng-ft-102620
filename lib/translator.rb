8f1579ae0150d21383f781a04aec06c6ba9abccd
Chelsey <cswilik@gmail.com>
2020-10-13 17:00:20 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,10 +7,7 @@ require 'pry'
 def load_library(file_path)
   file_path = YAML.load_file('lib/emoticons.yml')
   file_path.each_with_object({}) do |(key, value), final_array|
-    # binding.pry
-    final_array[key] = { } 
-    value.each do |emoticon|
-      value[]
+    if !final_array|key|
       binding.pry
     end 
   end 
