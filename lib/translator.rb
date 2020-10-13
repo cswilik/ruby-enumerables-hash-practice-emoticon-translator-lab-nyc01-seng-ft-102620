9b2cb810af2472ae5d5593211ac04728ed44805c
Chelsey <cswilik@gmail.com>
2020-10-13 17:23:14 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,7 +17,6 @@ end
 def get_english_meaning(file_path, emoticon)
   emotions = load_library(file_path)
   emotions.each do |key, value|
-    binding.pry
     if emotions[key][:japanese] == emoticon 
       return key
     end 
