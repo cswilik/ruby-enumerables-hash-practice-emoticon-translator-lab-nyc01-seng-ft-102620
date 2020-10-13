b89f62ed3fdf78daa2915565d3ad1d3941b84607
Chelsey <cswilik@gmail.com>
2020-10-13 16:49:10 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,8 +8,8 @@ def load_library(file_path)
   file_path = YAML.load_file('lib/emoticons.yml')
   file_path.each_with_object({}) do |(key, value), final_array|
     value.each do |emoticon|
+      binding.pry
       if emoticon.odd?
-        binding.pry
         puts emoticon
       end 
     end 
