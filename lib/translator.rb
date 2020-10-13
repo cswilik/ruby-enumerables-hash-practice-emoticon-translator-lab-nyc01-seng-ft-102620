d34f543f1586f42a96397c4833467e6b0fc4f9e9
Chelsey <cswilik@gmail.com>
2020-10-13 17:27:33 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,7 +21,7 @@ def get_english_meaning(file_path, emoticon)
       return key
     end
   end 
-  return "Sorry that emoticon was not found"
+  return "Sorry, that emoticon was not found"
 end
 
 def get_japanese_emoticon
