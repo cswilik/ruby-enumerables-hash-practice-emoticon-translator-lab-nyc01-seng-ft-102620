435967e64f416cb7d5c0bf54a3c6844cb8eb5864
Chelsey <cswilik@gmail.com>
2020-10-13 17:03:56 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,7 +8,7 @@ def load_library(file_path)
   file_path = YAML.load_file('lib/emoticons.yml')
   file_path.each_with_object({}) do |(key, value), final_array|
     if !final_array[key]
-      final_array[key] = 
+      final_array[key] = {:english => value[0], :japanese => value[1]}
       binding.pry
     end 
   end 
